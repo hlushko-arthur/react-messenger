dc501fa3840c736b487c01c4103d3344a10f3c83
Arthur Hlushko <hlushko.arthur@gmail.com>
2026-02-09 16:53:32 +0200

    added basic stylistic eslint configuration

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,6 +16,37 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/semi-spacing': 'error',
+      '@stylistic/keyword-spacing': ['error', { before: true, after: true }],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
+      '@stylistic/padded-blocks': ['error', { blocks: 'never' }],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/lines-between-class-members': 'error',
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: '*',
+        },
+        {
+          blankLine: 'never',
+          prev: 'import',
+          next: 'import',
+        },
+        {
+          blankLine: 'never',
+          prev: ['case', 'default'],
+          next: '*',
+        },
+      ],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
