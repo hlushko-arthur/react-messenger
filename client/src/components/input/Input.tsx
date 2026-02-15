3f053ce0154b0aaffa6318225d4a1334ddad6159
Arthur Hlushko <hlushko.arthur@gmail.com>
2026-02-15 20:55:58 +0200

    added messenger component

--- client/src/components/input/Input.tsx
+++ client/src/components/input/Input.tsx
@@ -2,32 +2,57 @@ import React from "react";
 import './Input.scss';
 import eyeIcon from '../../assets/icons/eye.png';
 import eyeclosedIcon from '../../assets/icons/eye-closed.png';
+import crossMark from '../../assets/icons/cross-mark.png';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	width?: string;
 	label?: string;
 	value?: string;
+	clearable?: boolean;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input: React.FC<InputProps> = (props: InputProps) => {
 	const [isShowPassword, setIsShowPassword] = React.useState(false);
 
+	const classes = `input ${props.placeholder ? 'input_without-label' : ''}`;
+
 	const togglePasswordVisibility = () => {
 		setIsShowPassword(prev => !prev);
+	};
+
+	const clearValue = () => {
+		if (!props.onChange) return;
+
+		const event = {
+			target: { value: "" },
+		} as React.ChangeEvent<HTMLInputElement>;
+
+		props.onChange(event);
 	};
 
 	return (
 		<div className="input-wrapper" style={{ width: props.width }}>
 			<input
 				type={isShowPassword ? 'text' : props.type || "text"}
-				placeholder=""
+				className={classes}
+				placeholder={props.placeholder}
 				name={props.name}
 				value={props.value}
 				onChange={props.onChange}
-				style={{ width: props.width }}
+				style={{ width: props.width, height: props.height}}
 			/>
-			<label>{props.label}</label>
+			{
+				props.clearable &&
+				props.value &&
+				<div className="clear-wrapper">
+					<img onClick={clearValue} src={crossMark} />
+				</div>
+			}
+			{
+				!props.placeholder &&
+				<label>{props.label}</label>
+			}
 			{
 				props.type === "password" &&
 				<span onClick={togglePasswordVisibility} className="password-toggle">
@@ -36,6 +61,10 @@ const Input: React.FC<InputProps> = (props: InputProps) => {
 			}
 		</div>
 	);
+};
+
+(Input as any).defaultProps = {
+	clearable: true,
 };
 
 export default Input;
--- client/src/pages/messenger/Messenger.tsx
+++ client/src/pages/messenger/Messenger.tsx
@@ -1,39 +1,93 @@
-import React from "react";
-import "./Messenger.scss";
+import React, { useEffect, useState } from "react";
+import Input from "../../components/input/Input";
 import userAvatar from "../../assets/icons/user.png";
+import "./Messenger.scss";
 
-chats;
+const Messenger: React.FC = () => {
+	const [activeTab, setActiveTab] = useState<'primary' | 'archived'>('primary');
 
-class Messenger extends React.Component {
-	render() {
-		return (
-			<div data-component='messenger' className="container">
-				<div className="aside">
-					<div className="tabs">
-						<div className="tabs_item active">Primary</div>
-						<div className="tabs_item">Archived</div>
-					</div>
+	const [searchText, setSearchText] = useState('');
 
-					<div className="chat-list">
-						<div className="chat-list_item">
-							<div className="chat-list_item_avatar">
-								<img src={userAvatar}></img>
-							</div>
-							<div className="chat-list_item_title">yulechka</div>
-							<div className="chat-list_item_last-message">
-								<div className="chat-list_item_last-message_text">Пук</div>
-								<div className="chat-list_item_last-message_date">1w</div>
+	return (
+		<div data-component='messenger' className="container">
+			<div className="aside">
+				<div className="search">
+					<Input clearable value={searchText} onChange={(e) => setSearchText(e.target.value)} placeholder="Search..." width="100%" />
+				</div>
+
+				{
+					!searchText &&
+					<>
+						<div className="tabs">
+							<div onClick={() => setActiveTab('primary')} className={`tabs_item ${activeTab === 'primary' ? 'active' : ''}`}>Primary</div>
+							<div onClick={() => setActiveTab('archived')} className={`tabs_item ${activeTab === 'archived' ? 'active' : ''}`}>Archived</div>
+						</div>
+
+						<div className="chat-list">
+							<div className="chat-list_item">
+								<div className="chat-list_item_avatar">
+									<img src={userAvatar}></img>
+								</div>
+								<div className="chat-list_item_title">yulechka</div>
+								<div className="chat-list_item_last-message">
+									<div className="chat-list_item_last-message_text">Пук</div>
+									<div className="chat-list_item_last-message_date">1w</div>
+								</div>
 							</div>
 						</div>
+					</>
+				}
+
+				{
+					searchText &&
+					<div className="global_search">
+						<div>
+							global search
+						</div>
+					</div>
+				}
+			</div>
+
+			<div className="content">
+				<div className="user">
+					<div className="user_avatar">
+						<img src={userAvatar}></img>
+					</div>
+					<div className="user_name">
+							Зозулікк
+					</div>
+					<div className="user_last-seen">
+							last seen yesterday at 10:06 PM
 					</div>
 				</div>
 
-				<div className="content">
-					content
+				<div className="chat">
+					<div className="chat_message from">
+						<div className="chat_message_text">
+								А з чим бургер
+						</div>
+						<div className="chat_message_date">
+							 	11:10 AM
+						</div>
+					</div>
+
+					<div className="chat_message to">
+						<div className="chat_message_text">
+								З ковбасою
+						</div>
+						<div className="chat_message_date">
+								10:11 AM
+						</div>
+					</div>
+
+				</div>
+
+				<div className="input-message">
+					<Input height="60px" placeholder="Type a message..." width="100%" />
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Messenger;
